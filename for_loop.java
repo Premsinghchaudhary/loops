0d71ac6706c447e017fbeb8c3787bc026145f12a
Prem singh <107177531+Premsinghchaudhary@users.noreply.github.com>
2022-06-18 19:23:09 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class for_loop
+{
+	public static void main (String agrs[] ) 
+	{
+		
+		int i;  // declartion varaiable  and datatype is int
+		// using for loop
+		for(i=1;i<790;i++) 
+		{
+			System.out.print("\t"+i); //print the * 
+			
+		}
+		
+	}
+}
